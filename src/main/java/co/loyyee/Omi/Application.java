131774ed67c354f5ae6b8dc27cdd51e987f61a1d
Loy Yee Ko <koloyyee@gmail.com>
2024-02-28 18:41:46 -0500

    @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })

--- src/main/java/co/loyyee/Omi/Application.java
+++ src/main/java/co/loyyee/Omi/Application.java
@@ -3,10 +3,11 @@ package co.loyyee.Omi;
 import co.loyyee.Omi.config.DrafterConfigurationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
 @EnableConfigurationProperties(DrafterConfigurationProperties.class)
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class Application {
 
 	public static void main(String[] args) {
