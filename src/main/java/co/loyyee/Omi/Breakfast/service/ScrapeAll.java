55106b2c30c948ea491272ec69f91e85ce9be30c
Loy Yee Ko <koloyyee@gmail.com>
2024-03-07 09:16:40 -0500

    stop on sql init mode

--- src/main/java/co/loyyee/Omi/Breakfast/service/ScrapeAll.java
+++ src/main/java/co/loyyee/Omi/Breakfast/service/ScrapeAll.java
@@ -9,21 +9,21 @@ import java.util.concurrent.*;
 
 /**
  * A multithreaded scraping all started.
- * */
+ */
 public class ScrapeAll {
     final static private Logger log = LoggerFactory.getLogger(ScrapeAll.class);
 
     public static void main(String[] args) {
-        String[] tickers = new String[]{"AAPL", "GOOG", "META", "BRK-A"};
+        String[] tickers = new String[] { "AAPL", "GOOG", "META", "BRK-A" };
         ArrayList<Finviz> fscrapers = new ArrayList<>();
         ArrayList<Benzinga> bscrapers = new ArrayList<>();
 
-        for(String ticker : tickers) {
-//            fscrapers.add(new Finviz(ticker));
+        for (String ticker : tickers) {
+            // fscrapers.add(new Finviz(ticker));
             bscrapers.add(new Benzinga(ticker));
         }
-//        List<Finviz> combinedList = new ArrayList<>(fscrapers);
-        for(Benzinga b : bscrapers)  {
+        // List<Finviz> combinedList = new ArrayList<>(fscrapers);
+        for (Benzinga b : bscrapers) {
             try {
                 b.getThread().join();
                 log.info(b.getTickerNewsList().toString());
@@ -32,31 +32,31 @@ public class ScrapeAll {
             }
         }
 
-//        for(Finviz f : fscrapers) {
-//            try {
-//                f.getThread().join();
-//                log.info(f.getTickerNewsList().toString());
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//                throw new RuntimeException(e);
-//            }
-//        }
+        // for(Finviz f : fscrapers) {
+        // try {
+        // f.getThread().join();
+        // log.info(f.getTickerNewsList().toString());
+        // } catch (InterruptedException e) {
+        // e.printStackTrace();
+        // throw new RuntimeException(e);
+        // }
+        // }
     }
+
     public static void vThread() throws InterruptedException, ExecutionException {
-        String[] tickers = new String[]{"AAPL", "GOOG", "META", "BRK-A"};
+        String[] tickers = new String[] { "AAPL", "GOOG", "META", "BRK-A" };
         List<Callable<Finviz>> fscrapers = new ArrayList<>();
         List<Callable<Benzinga>> bscrapers = new ArrayList<>();
 
-        for(String ticker : tickers) {
-            fscrapers.add(()->new Finviz(ticker));
-            bscrapers.add(()->new Benzinga(ticker));
+        for (String ticker : tickers) {
+            fscrapers.add(() -> new Finviz(ticker));
+            bscrapers.add(() -> new Benzinga(ticker));
         }
 
         ExecutorService service = Executors.newVirtualThreadPerTaskExecutor();
-        for(Future<Finviz> f : service.invokeAll(fscrapers)) {
+        for (Future<Finviz> f : service.invokeAll(fscrapers)) {
             Finviz finviz = f.get();
             finviz.getTickerNewsList().forEach(System.out::println);
         }
     }
 }
-
