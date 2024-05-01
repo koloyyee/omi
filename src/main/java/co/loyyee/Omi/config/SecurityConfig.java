3a4db7c787dcd991bd2de61b76afb85080df62d6
Loy Yee Ko <koloyyee@gmail.com>
2024-05-01 13:36:23 -0400

    auth private test established

--- src/main/java/co/loyyee/Omi/config/SecurityConfig.java
+++ src/main/java/co/loyyee/Omi/config/SecurityConfig.java
@@ -37,23 +37,25 @@ public class SecurityConfig {
 
   private static final Logger log = LoggerFactory.getLogger(SecurityConfig.class);
   private final DrafterRsaKeyProperties drafterRsaKey;
-	
-	public SecurityConfig(DrafterRsaKeyProperties drafterRsaKey) {
-		this.drafterRsaKey = drafterRsaKey;
-	}
-	
-	@Bean
+
+  public SecurityConfig(DrafterRsaKeyProperties drafterRsaKey) {
+    this.drafterRsaKey = drafterRsaKey;
+  }
+
+  @Bean
   SecurityFilterChain drafterSecurityFilterChain(HttpSecurity http) throws Exception {
-    return http
-        .csrf(csrf -> csrf.disable())
-        .authorizeHttpRequests(auth ->
-            auth
-                .requestMatchers("/drafter/public/**").permitAll()
-                .requestMatchers("/drafter/private/**").authenticated()
-//                .anyRequest().authenticated()
-        )
+    return http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers("/drafter/public/**")
+                    .permitAll()
+                    .requestMatchers("/drafter/private/**")
+                    .authenticated()
+            //                .anyRequest().authenticated()
+            )
         .oauth2ResourceServer(oauth -> oauth.jwt(Customizer.withDefaults()))
-        .sessionManagement( session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .sessionManagement(
+            session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .httpBasic(Customizer.withDefaults())
         .formLogin(Customizer.withDefaults())
         .build();
@@ -64,13 +66,16 @@ public class SecurityConfig {
    * Register user will be done in the similar fashion but UserDTO or User Model is needed.
    */
   @Bean
-  JdbcUserDetailsManager drafterDefaultUser(
-      @Qualifier("drafterDataSource") DataSource dataSource) throws SQLException {
+  JdbcUserDetailsManager drafterDefaultUser(@Qualifier("drafterDataSource") DataSource dataSource)
+      throws SQLException {
 
     JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
     if (!manager.userExists("david")) {
       UserDetails admin =
-          User.withUsername("david").password(passwordEncoder().encode("password")).roles("ADMIN").build();
+          User.withUsername("david")
+              .password(passwordEncoder().encode("password"))
+              .roles("ADMIN")
+              .build();
       manager.createUser(admin);
     }
 
@@ -81,22 +86,23 @@ public class SecurityConfig {
   PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder();
   }
-  
-  
+
   /**
    * JWT setup tutorial <br>
-   * <a href="https://www.danvega.dev/blog/spring-security-jwt"> Dan Vega's Spring Security JWT (backend only)</a>
-   * */
-  
-  
+   * <a href="https://www.danvega.dev/blog/spring-security-jwt">Dan Vega's Spring Security JWT
+   * (backend only)</a>
+   */
   @Bean
-  JwtDecoder jwtDecoder(){
+  JwtDecoder jwtDecoder() {
     return NimbusJwtDecoder.withPublicKey(drafterRsaKey.publicKey()).build();
   }
-  
+
   @Bean
   JwtEncoder jwtEncoder() {
-    JWK jwk = new RSAKey.Builder(drafterRsaKey.publicKey()).privateKey(drafterRsaKey.privateKey()).build();
+    JWK jwk =
+        new RSAKey.Builder(drafterRsaKey.publicKey())
+            .privateKey(drafterRsaKey.privateKey())
+            .build();
     JWKSource<SecurityContext> jwks = new ImmutableJWKSet<>(new JWKSet(jwk));
     return new NimbusJwtEncoder(jwks);
   }
