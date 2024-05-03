ebd45dbb60a8a2c0cdd1dc3b456980a38b5e434b
Loy Yee Ko <koloyyee@gmail.com>
2024-05-03 07:30:25 -0400

    remove log and added debug

--- src/main/java/co/loyyee/Omi/Drafter/controller/DraftPublicController.java
+++ src/main/java/co/loyyee/Omi/Drafter/controller/DraftPublicController.java
@@ -123,7 +123,6 @@ public class DraftPublicController {
       String finishReason = resp.getResult().getMetadata().getFinishReason();
       if (finishReason.equalsIgnoreCase("stop")) {
         String content = resp.getResult().getOutput().getContent();
-        System.out.println(resp.getResults());
         return ResponseEntity.ok(content);
       } else {
         if (finishReason.equalsIgnoreCase("length")) {
